7e70cf015828db1e450e3548763e2a040553a9d1
stian <stiantrondsen@hotmail.com>
2021-01-16 14:38:31 +0100

    Task1a,  common

---
+++ Common/LatestRate/LatestRateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.LatestRate
+{
+    public class LatestRateResponse
+    {
+        public bool Success { get; set; }
+        public ulong Timestamp { get; set; }
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public IDictionary<Currency, decimal> Rates { get; set; }
+    }
+
+    public record LatestRate(bool Success, ulong Timestamp, Currency Base, DateTime Date, IDictionary<Currency, decimal> Rates);
+}
---
+++ Task1a/Program.cs
@@ -0,0 +1,21 @@
+﻿using Common.LatestRate;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Task1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var dummyResponse = File.ReadAllText("response.json");
+
+            var res = JsonConvert.DeserializeObject<LatestRate>(dummyResponse);
+
+
+            Console.ReadKey();
+        }
+    }
+}
