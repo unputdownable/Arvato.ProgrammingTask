b1e2788b318d5a3ce39566bc89593db81d53b04f
stian <stiantrondsen@hotmail.com>
2021-01-18 23:32:25 +0100

    added task1c

---
+++ Task1c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
