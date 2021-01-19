ae8c4c734c799ee34242ef189cc2f3e9c0ebc715
stian <stiantrondsen@hotmail.com>
2021-01-19 15:54:36 +0100

    Task2a done

---
+++ Task2.API/Controllers/ConvertController.cs
@@ -0,0 +1,60 @@
+﻿using Common.Database;
+using Common.Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ConvertController : ControllerBase
+    {
+        private readonly RatesContext context;
+
+        public ConvertController(RatesContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Convert(string from, string to, decimal amount, DateTime? date = null)
+        {
+            if (from is null || to is null)
+            {
+                return BadRequest("One or more symbols missing");
+            }
+
+            var currencyFrom = await context.Currencies.Include(c => c.Rates).SingleOrDefaultAsync(c => c.Symbol == from);
+            var currencyTo = await context.Currencies.Include(c => c.Rates).SingleOrDefaultAsync(c => c.Symbol == to);
+            
+            if (currencyFrom is null || currencyTo is null)
+            {
+                return BadRequest("One or more symbols unsupported");
+            }
+
+            Rate rateFrom;
+            Rate rateTo;
+            if (date is null)
+            {
+                rateFrom = currencyFrom.Rates.OrderBy(r => r.Date).LastOrDefault();
+                rateTo = currencyTo.Rates.OrderBy(r => r.Date).LastOrDefault();
+            }
+            else
+            {
+                rateFrom = currencyFrom.Rates.SingleOrDefault(r => r.Date == date);
+                rateTo = currencyFrom.Rates.SingleOrDefault(r => r.Date == date);
+            }
+
+            var convertedAmount = (amount / rateFrom?.Value) * rateTo?.Value;
+
+            return Ok(convertedAmount);
+        }
+
+    }
+}
---
+++ Task2.API/Startup.cs
@@ -0,0 +1,59 @@
+using Common.Database;
+using Common.Database.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Task2.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<RatesContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Rates"));
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task2.API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task2.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
